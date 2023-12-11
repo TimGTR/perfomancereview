cd0652af02d4b0093a371c841bcccac505fd6ec3
Tim <tr.garipov@yandex.ru>
2023-12-11 10:41:11 +0300

    streamApi

---
+++ src/main/java/ru/ubrr/pr/streamapi/s_4_6_2/StreamApiExample.java
@@ -0,0 +1,35 @@
+package ru.ubrr.pr.streamapi.s_4_6_2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class StreamApiExample {
+    public static void main(String[] args) {
+        List<String> stringsList = Arrays.asList("apple", "banana", "orange");
+        Stream<String> stringStream = stringsList.stream();
+
+        String[] stringArray = {"apple", "banana", "orange"};
+        Stream<String> stringStreamFromArray = Arrays.stream(stringArray);
+
+        Stream<String> stringStreamOf = Stream.of("apple", "banana", "orange");
+
+        String filePath = "path/to/your/file.txt";
+        try (Stream<String> linesStream = Files.lines(Paths.get(filePath))) {
+            linesStream.forEach(System.out::println); // Выводим содержимое файла на экран
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String text = "Hello, World!";
+        text.chars().forEach(System.out::println);
+
+        String fruitsText = "apple,banana,orange";
+        Pattern pattern = Pattern.compile(",");
+        Stream<String> fruitsStream = pattern.splitAsStream(fruitsText);
+    }
+}
